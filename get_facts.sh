1f8bd26dd5f2eb538b36c7dec95ad48b976a6b30
Maneesha Sane <ahseenam@gmail.com>
2025-05-14 16:24:27 -0400

    try adding in css

--- get_facts.sh
+++ get_facts.sh
@@ -1,6 +1,15 @@
+echo "<!DOCTYPE html>" > index.html 
+echo "<html>" >> index.html 
+echo "<head>" >> index.html 
+echo "<style>" >> index.html 
+echo "body {background-color: powderblue;}" >> index.html 
+echo "h1   {color: blue;}" >> index.html 
+echo "p    {color: red;}" >> index.html 
+echo "</style>" >> index.html 
+echo "</head>" >> index.html 
+echo "<body>" >> index.html 
 
-
-echo "<h1>Fun facts</h1>" > index.html
+echo "<h1>Fun facts</h1>" >> index.html
 echo "<ul>" >> index.html
 
 for file in data/*.json; do 
@@ -14,4 +23,8 @@ echo "</ul>" >> index.html
 
 python3 myscript.py >> index.html 
 
+echo "<img src='my_plot.png'>" >> index.html
 
+echo "</body>" >> index.html 
+
+echo "</html>" >> index.html 
