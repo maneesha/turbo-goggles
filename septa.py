fd4a7cb97adefaef2cd79844d5f8be4aeade20d7
Maneesha Sane <ahseenam@gmail.com>
2025-05-28 17:04:36 -0400

    more clean up

--- septa.py
+++ septa.py
@@ -1,7 +1,6 @@
 import requests
-import pprint
-import pandas as pd 
-import html 
+import pandas as pd
+import html
 
 url = 'https://www3.septa.org/api/TrainView/index.php'
 headers = {
@@ -18,33 +17,39 @@ except requests.exceptions.RequestException as e:
 
 df = pd.DataFrame(data)
 
+
 def location_url(lat, lon):
     url = f"https://www.google.com/maps/search/?api=1&query={lat},{lon}"
     link_text = "View on Google Maps"
     return f'<a href="{url}" target="_blank">{link_text}</a>'
 
+
 def count_cars(car_list):
     car_count = car_list.split(",")
     return len(car_count) if isinstance(car_count, list) else 0
 
+# Return count of cars instead of list of car numbers
 df['count_cars'] = df['consist'].apply(count_cars)
+# Convert lat/lon to Google Maps URL
 df['location_url'] = df.apply(lambda row: location_url(row['lat'], row['lon']), axis=1)
 
-lat = df.iloc[0]['lat']
-lon = df.iloc[0]['lon']
-
+# Limit colums 
 cols = ['SOURCE', 'currentstop',  'nextstop', 'dest', 'trainno', 'line', 'late', 'count_cars', 'location_url']
-
 df = df[cols]
+
+# Convert dataframe to HTML table 
 data_table = df.to_html(table_id="trains", index=False)
 data_table = html.unescape(data_table)
 
+# Retrieve page template as string 
 page_template = "septa_table_template.html"
 with open(page_template, 'r') as template_file:
     template_content = template_file.read()
 
+# Replace placeholder in template with html table
 html_content = template_content.replace("train_table_goes_here", data_table)
 
+# Save the final HTML content to a file
 file = "result2.html"
 with open(file, "w") as file:
     file.write(html_content)
